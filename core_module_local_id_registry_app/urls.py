b8765c4eb2fff331382c325ad9c4903789dd6701
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-06-19 17:04:48 -0400

    chore: optimize imports
    Change-Id: I406ad29383c641e73954160af023b3604796c9a2

--- core_module_local_id_registry_app/urls.py
+++ core_module_local_id_registry_app/urls.py
@@ -1,6 +1,7 @@
 """ Url router for the local id registry module
 """
 from django.conf.urls import url
+
 from core_module_local_id_registry_app.views.views import LocalIdRegistryModule
 
 urlpatterns = [
