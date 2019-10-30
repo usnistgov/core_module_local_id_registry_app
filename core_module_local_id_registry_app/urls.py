6ef4f03970006cddf475e60fe2532f7efe8316d2
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-30 10:16:32 -0400

    feat: upgrade django 2.2
    Change-Id: Ia50410824ecf233c2d392ac55481475bf8956444

--- core_module_local_id_registry_app/urls.py
+++ core_module_local_id_registry_app/urls.py
@@ -1,9 +1,10 @@
 """ Url router for the local id registry module
 """
-from django.conf.urls import url
+
+from django.urls import re_path
 
 from core_module_local_id_registry_app.views.views import LocalIdRegistryModule
 
 urlpatterns = [
-    url(r'module-local-id-registry', LocalIdRegistryModule.as_view(), name='core_module_local_id_registry_view'),
+    re_path(r'module-local-id-registry', LocalIdRegistryModule.as_view(), name='core_module_local_id_registry_view'),
 ]
