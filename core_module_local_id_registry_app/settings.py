a575923a8f98798184d7f87ac44574c5e71cbde4
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-06-26 09:06:54 -0400

    feat: add linked records support
    Change-Id: Ibdb5249ca1c7940df455a2dc01ff4aa1f3eee63d

--- core_module_local_id_registry_app/settings.py
+++ core_module_local_id_registry_app/settings.py
@@ -8,3 +8,7 @@ if not settings.configured:
 LOCAL_ID_LENGTH = getattr(settings, "LOCAL_ID_LENGTH", 20)
 """ int: length used to generate the unique local id.
 """
+
+INSTALLED_APPS = getattr(settings, "INSTALLED_APPS", [])
+""" :py:class:`list`: List of apps installed.
+"""
--- core_module_local_id_registry_app/views/views.py
+++ core_module_local_id_registry_app/views/views.py
@@ -1,7 +1,7 @@
 """ Local Id Registry Module
 """
 from core_main_registry_app.components.data.api import generate_unique_local_id
-from core_module_local_id_registry_app.settings import LOCAL_ID_LENGTH
+from core_module_local_id_registry_app.settings import LOCAL_ID_LENGTH, INSTALLED_APPS
 from core_parser_app.tools.modules.views.builtin.input_module import AbstractInputModule
 
 
@@ -9,7 +9,13 @@ class LocalIdRegistryModule(AbstractInputModule):
     def __init__(self):
         """ Initialize module
         """
-        AbstractInputModule.__init__(self, disabled=True)
+        placeholder = (
+            "A PID will be generated for this resource"
+            if "core_linked_records_app" in INSTALLED_APPS
+            else None
+        )
+
+        AbstractInputModule.__init__(self, disabled=True, placeholder=placeholder)
 
     def _retrieve_data(self, request):
         """ Retrieve module's data
@@ -25,7 +31,10 @@ class LocalIdRegistryModule(AbstractInputModule):
             if "data" in request.GET:
                 data = request.GET["data"]
             else:
-                data = generate_unique_local_id(LOCAL_ID_LENGTH)
+                if "core_linked_records_app" in INSTALLED_APPS:
+                    data = ""
+                else:
+                    data = generate_unique_local_id(LOCAL_ID_LENGTH)
             self.default_value = data
         elif request.method == "POST":
             if "data" in request.POST:
